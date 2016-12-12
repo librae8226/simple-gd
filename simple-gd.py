7d7b760dd777b52a24ae3dadabad0d43ed8187f6
Librae <librae8226@gmail.com>
2016-12-13 01:49:08 +0800

    fix to use adjusted eps rather than raw eps

--- simple-gd.py
+++ simple-gd.py
@@ -9,17 +9,17 @@ from scipy.stats import norm
 N = 12*5
 POOL = [
     # in basket
-    #'600887.XSHG', # 伊利股份
     '002415.XSHE', # 海康威视
     '600422.XSHG', # 昆药集团
-    #'600104.XSHG', # 上汽集团
     '600594.XSHG', # 益佰制药
     '601633.XSHG', # 长城汽车
+    # target
+    #'600887.XSHG', # 伊利股份
+    '600104.XSHG', # 上汽集团
     #'000651.XSHE', # 格力电器
     #'601668.XSHG', # 中国建筑
     #'600690.XSHG', # 青岛海尔
-    #'600048.XSHG', # 保利地产
-    # target
+    '600048.XSHG', # 保利地产
     #'600030.XSHG', # 中信证券
     #'601318.XSHG', # 中国平安
     '601222.XSHG', # 林洋
@@ -27,96 +27,23 @@ POOL = [
     #'000538.XSHE', # 云南白药
     '002241.XSHE', # 歌尔声学
     #'601231.XSHG', # 环旭电子
-    #'601877.XSHG', # 正泰电器
+    '601877.XSHG', # 正泰电器
     #'600332.XSHG', # 白云山
     #'000963.XSHE', # 华东医药
     #'600177.XSHG', # 雅戈尔
-    #'600066.XSHG',
-    #'600377.XSHG',
+    '600315.XSHG', # 上海家化
+    '600066.XSHG', # 宇通客车
 ]
 
-def estimation_formula_bg_dynamic(growth, eps, pe):
-    """
-    BG formula, integrate with the normal pe (based on Gaussian Distribution)
-    """
-    return (2*growth+pe)*eps
+POOL_watch = [
+    # watch
+    '000826.XSHE', # 启迪桑德
+    '000590.XSHE', # 启迪古汉
+]
 
-def estimation_formula_bg(growth, eps):
-    """
-    The stock price estimation formula suggested by Benjamin Graham
-    According to "The Intelligent Investor"
-    """
-    return (2*growth+8.5)*eps
-
-# growth rate in N years
-NYG = 3
-def est(security, date, pe_mu=8.5, pe_std=0):
-    """
-    """
-    np = []
-    delta = 0
-    # collect net profit in last NYG years, and work out a average growth rate
-    for i in range(0, NYG+1):
-        d = add_months(date, -i*12)
-        tmp_np = get_net_profit(security, d)
-        if math.isnan(tmp_np):
-            date = add_months(date, -3)
-            d = add_months(date, -i*12)
-            np.append(get_net_profit(security, d))
-        else:
-            np.append(tmp_np)
-        #log.debug("[%s] np at %s: %.2f", security, d.strftime("%Y-%m-%d"), np[i])
-        if i != 0:
-            delta += (np[i-1] - np[i])/np[i]
-            #log.debug("[%s] at %s, growth: %.2f", security, add_months(date, -(i-1)*12).strftime("%Y-%m-%d"), (np[i-1] - np[i])/np[i])
-    growth = delta/NYG
-
-    eps = get_eps(security, date)
-    if math.isnan(eps):
-        eps = get_eps(security, add_months(date, -3))
-
-    log.debug("[%s] growth: %.4f, eps: %.2f", security, growth, eps)
-    est = estimation_formula_bg(growth, eps)
-    est_left = estimation_formula_bg_dynamic(growth, eps, pe_mu-pe_std)
-    est_centrum = estimation_formula_bg_dynamic(growth, eps, pe_mu)
-    est_right = estimation_formula_bg_dynamic(growth, eps, pe_mu+pe_std)
-    return est, est_left, est_centrum, est_right
-
-def get_net_profit(security, date):
-    """
-    """
-    np = 0
-    y, q = get_last_quarters(date)
-    res = query(
-        valuation.code, income.net_profit
-        ).filter(
-        valuation.code == security
-    )
-    for i in range(1, 5):
-        ret = get_fundamentals(res, statDate=str(y[i])+'q'+str(q[i]))
-        #log.info(ret)
-        np += ret['net_profit'].mean()
-        #log.debug("%dq%d 1/4 np: %.2f", y[i], q[i], ret['net_profit'].mean())
-    return np
-
-def get_eps(security, date):
-    """
-    """
-    e = 0
-    y, q = get_last_quarters(date)
-    res = query(
-        valuation.code, valuation.day, income.statDate, valuation.pe_ratio, valuation.pb_ratio, income.basic_eps, income.diluted_eps
-        ).filter(
-        valuation.code == security
-    )
-    for i in range(1, 5):
-        ret = get_fundamentals(res, statDate=str(y[i])+'q'+str(q[i]))
-        #log.info(ret)
-        e += ret['basic_eps'].mean()
-        #e += ret['diluted_eps'].mean()
-        #log.debug("%dq%d 1/4 eps: %.2f", y[i], q[i], ret['basic_eps'].mean())
-    #log.debug("eps: %.2f", e)
-    return e
+POOL_test = [
+    '601222.XSHG', # 林洋
+]
 
 def get_last_quarters(date):
     """
@@ -160,7 +87,115 @@ def add_months(sourcedate, months):
     day = min(sourcedate.day,calendar.monthrange(year,month)[1])
     return datetime.datetime(year,month,day)
 
-def get_pe_of_period(security, start, end):
+def estimation_formula_bg_dynamic(growth, eps, pe):
+    """
+    BG formula, integrate with the normal pe (based on Gaussian Distribution)
+    """
+    return (2*growth+pe)*eps
+
+def estimation_formula_bg(growth, eps):
+    """
+    The stock price estimation formula suggested by Benjamin Graham
+    According to "The Intelligent Investor"
+    """
+    return (2*growth+8.5)*eps
+
+# growth rate in N years
+NYG = 3
+def est(security, date, pe_mu=8.5, pe_std=0):
+    """
+    """
+    np = []
+    delta = 0
+    # collect net profit in last NYG years, and work out a average growth rate
+    for i in range(0, NYG+1):
+        d = add_months(date, -i*12)
+        tmp_np = get_net_profit(security, d)
+        if math.isnan(tmp_np):
+            date = add_months(date, -3)
+            d = add_months(date, -i*12)
+            np.append(get_net_profit(security, d))
+        else:
+            np.append(tmp_np)
+        #log.debug("%s np: %.2f", d.strftime("%Y-%m-%d"), np[i])
+        if i != 0:
+            delta += (np[i-1] - np[i])/np[i]
+            #log.debug("%s growth: %.2f", add_months(date, -(i-1)*12).strftime("%Y-%m-%d"), (np[i-1] - np[i])/np[i])
+    growth = delta/NYG
+
+    eps = 0
+    cap = 1
+    # collect average eps in last NYG years
+    for i in range(0, NYG):
+        d = add_months(date, -i*12)
+        factor = 1
+        df = get_fundamentals(query(
+            valuation.code,
+            valuation.capitalization
+        ).filter(
+            valuation.code == security
+        ), date = d.strftime("%Y-%m-%d"))
+        if i == 0:
+            cap = float(df['capitalization'])
+        else:
+            factor = float(df['capitalization'])/cap
+        e = get_eps(security, d)*factor
+        #log.debug("%d %s cap: %d, factor: %f, eps: %.2f", i, d.strftime("%Y-%m-%d"), df['capitalization'], factor, e)
+        eps = eps + e
+    eps = eps/NYG
+
+    log.info("growth: %.4f, eps: %.2f", growth, eps)
+    est = estimation_formula_bg(growth, eps)
+    est_left = estimation_formula_bg_dynamic(growth, eps, pe_mu-pe_std)
+    est_centrum = estimation_formula_bg_dynamic(growth, eps, pe_mu)
+    est_right = estimation_formula_bg_dynamic(growth, eps, pe_mu+pe_std)
+    return est, est_left, est_centrum, est_right
+
+def get_net_profit(security, date):
+    """
+    """
+    np = 0
+    y, q = get_last_quarters(date)
+    res = query(
+        valuation.code, income.net_profit
+        ).filter(
+        valuation.code == security
+    )
+    for i in range(1, 5):
+        ret = get_fundamentals(res, statDate=str(y[i])+'q'+str(q[i]))
+        #log.info(ret)
+        np += ret['net_profit'].mean()
+        #log.debug("%dq%d %d/4 np: %.2f", y[i], q[i], i, ret['net_profit'].mean())
+    return np
+
+def get_eps(security, date):
+    """
+    """
+    y, q = get_last_quarters(date)
+    res = query(
+            valuation.code,
+            valuation.capitalization,
+            income.net_profit
+        ).filter(
+        valuation.code == security
+    )
+    np = 0
+    cap = 0
+    for i in range(1, 5):
+        ret = get_fundamentals(res, statDate=str(y[i])+'q'+str(q[i]))
+        np += ret['net_profit'].mean();
+        #log.info(ret)
+        #e += ret['basic_eps'].mean()
+        #e += ret['diluted_eps'].mean()
+        #log.debug("%dq%d 1/4 eps: %.2f", y[i], q[i], ret['basic_eps'].mean())
+        #log.debug("%dq%d 1/4 net profit: %d cap: %d", y[i], q[i], ret['net_profit'].mean(), ret['capitalization'])
+        if i == 4:
+            cap = ret['capitalization']*10000
+    eps = float(np) / cap
+    return eps
+
+def get_pe_in_period(security, start, end):
+    ''' old eps calc
     df = get_price(
         security,
         start_date = start.strftime("%Y-%m-%d"),
@@ -170,11 +205,32 @@ def get_pe_of_period(security, start, end):
     close = df['close']/df['factor']
     p = close.mean()
     #log.info('price mean: %f', p)
+    '''
+    df = get_fundamentals(query(
+                valuation.market_cap,
+                valuation.capitalization,
+                valuation.pe_ratio
+            ).filter(
+                valuation.code == security
+            ), date = end.strftime("%Y-%m-%d"))
+    #p = df['market_cap']
+    #log.debug('<%s> market_cap: %f', end.strftime("%Y-%m-%d"), p)
+    #log.debug('<%s> cap: %d', end.strftime("%Y-%m-%d"), int(df['capitalization']))
+
+    pe = float(df['pe_ratio'])
+
+    ''' old eps calc
     e = get_eps(security, end)
+    '''
+    ''' old net profit, pe calc
+    e = get_net_profit(security, end)
     if math.isnan(e):
-        #log.warn("get_pe_of_period, e is nan!")
+        #log.warn("get_pe_in_period, e is nan!")
         pass
-    pe = round(p/e, 2)
+
+    pe = round(p*10000*10000/e, 2)
+    log.debug('P/E Ratio of %s at %s: %.2f', security, end.strftime("%Y-%m"), pe)
+    '''
     #log.debug('P/E Ratio of %s at %s: %.2f', security, end.strftime("%Y-%m"), pe)
     return pe
 
@@ -209,7 +265,7 @@ def gd_init(context):
             else:
                 #print tmp['market_cap'].mean()
                 pass
-            pe = get_pe_of_period(security, first, last)
+            pe = get_pe_in_period(security, first, last)
             if math.isnan(pe) == False:
                 g.security_gd_pe[g.pool.index(security)][last.strftime("%Y-%m-%d")] = pe
 
@@ -236,7 +292,7 @@ def gd_init(context):
 def gd_update(context, security):
     curr = context.current_dt
     first, last = get_month_day_range(curr)
-    pe = get_pe_of_period(security, first, last)
+    pe = get_pe_in_period(security, first, last)
     if math.isnan(pe) == False:
         g.security_gd_pe[g.pool.index(security)][last.strftime("%Y-%m-%d")] = pe
 
@@ -286,8 +342,10 @@ def gd_pe_get_mu_std(security):
 
 def on_month_end(context):
     date = context.current_dt
-    log.info('on_month_end: ' + str(date))
+    log.debug('on_month_end: ' + str(date) + '\r\n')
     for security in g.pool:
+        log.info('[%s %s]', security, get_security_info(security).display_name)
+
         # update
         gd_update(context, security);
 
@@ -300,26 +358,31 @@ def on_month_end(context):
             fields = ['close', 'factor'])
         close = df['close']/df['factor']
         close = close.mean()
+        ''' old calc
         e = get_eps(security, date)
         if math.isnan(e):
             e = get_eps(security, add_months(date, -3))
         pe = round(close/e, 2)
-        log.debug('[%s] p/e ratio: %.2f @ price: %.2f', get_security_info(security).display_name, pe, close)
+        '''
+        pe = get_pe_in_period(security, date, date)
+        log.info('p/e ratio: %.2f @ price: %.2f', pe, close)
 
         # get p/e array mu and std
         mu, std = gd_pe_get_mu_std(security)
-        log.debug('[%s] p/e ratio mu: %.2f, std: %.2f', security, mu, std)
+        log.info('p/e ratio mu: %.2f, std: %.2f', mu, std)
 
         # estimation
         est_value, est_left, est_centrum, est_right = est(security, date, mu, std)
-        log.debug('[%s] estimated value: %.2f', security, est_value)
-        log.debug('[%s] dynamic est: %.2f~%.2f~%.2f', security, est_left, est_centrum, est_right)
-        log.debug('[%s] range from left: %.2f%%', security, (close-est_left)/est_left*100.0)
+        log.info('estimated value: %.2f', est_value)
+        log.info('dynamic est: %.2f~%.2f~%.2f', est_left, est_centrum, est_right)
+        log.info('range from left: %.2f%%', (close-est_left)/est_left*100.0)
+        log.info('position: %.2f%%', (close-est_left)/(est_right-est_left)*100.0)
+        log.info('----------------------------------------')
 
 def initialize(context):
     g.pool = POOL
-    log.info(g.pool)
-    log.info("initialize: amount of securities: %d", len(g.pool))
+    log.debug(g.pool)
+    log.debug("initialize: amount of securities: %d", len(g.pool))
 
     g.security_gd_pe = range(0, len(g.pool))
     for security in g.pool:
